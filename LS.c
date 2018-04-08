9610ab63a7444b43780f0d6df06a3dc74f90f8a1
imehulkhurana <38052074+imehulkhurana@users.noreply.github.com>
2018-04-08 15:58:37 +0530

    Update LS.c

--- LS.c
+++ LS.c
@@ -262,7 +262,7 @@ A:
         printf("\n1.Students ");
         printf("\n2.Teachers ");
         printf("\n3.Both at the same time ");
-        printf("\n4.Exit\n");
+        printf("\n4.Exit \n");
 scanf("\n%d",&ch);
 int a=0,a1=0;
 switch(ch)
